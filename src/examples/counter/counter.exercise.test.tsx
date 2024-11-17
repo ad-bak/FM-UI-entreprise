6bfdb66604844a0ec34b578df175303bf87d6933
Adnan <adbak.dev@gmail.com>
2024-11-17 20:02:21 +0100

    exercise

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,6 +1,9 @@
-import { screen, render } from '@testing-library/react';
+/// @vitest-environment happy-dom
+
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
+import { render } from './test/utilities';
 
 test('it should render the component', () => {
   render(<Counter />);
@@ -20,9 +23,31 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  const { user } = render(<Counter />);
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+  const counter = screen.getByTestId('current-count');
+  expect(counter).toBeDefined;
+  expect(counter).toHaveTextContent('0');
+});
+
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  const { user } = render(<Counter />);
+
+  const counter = screen.getByTestId('current-count');
+  expect(counter).toBeDefined;
+  expect(counter).toHaveTextContent('0');
+
+  const increaseBtn = screen.getByRole('button', { name: 'Increment' });
+  const resetBtn = screen.getByRole('button', { name: 'Reset' });
+
+  await user.click(increaseBtn);
+  await user.click(increaseBtn);
+  await user.click(increaseBtn);
+
+  expect(counter).toHaveTextContent('3');
+
+  await user.click(resetBtn);
+
+  expect(counter).toHaveTextContent('0');
+});
