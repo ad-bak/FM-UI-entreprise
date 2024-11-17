731f1e0581dae183c92f3086913ac864a7070149
Adnan <adbak.dev@gmail.com>
2024-11-17 20:04:28 +0100

    exercise

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -24,11 +24,11 @@ test('it should increment when the "Increment" button is pressed', async () => {
 });
 
 test('it should render the component with an initial count', () => {
-  const { user } = render(<Counter />);
+  const { user } = render(<Counter initialCount={123} />);
 
   const counter = screen.getByTestId('current-count');
   expect(counter).toBeDefined;
-  expect(counter).toHaveTextContent('0');
+  expect(counter).toHaveTextContent('123');
 });
 
 test('it should reset the count when the "Reset" button is pressed', async () => {
